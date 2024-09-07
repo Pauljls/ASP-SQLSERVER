4d586b9fc4c3f945bf86aa1bfd7e0e63449daada
Pauljls <paulazarsol@gmail.com>
2024-09-06 19:06:37 -0500

    CREACION DE BD Y MODELO USUARIO

---
+++ Registro/Models/UsuarioModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Registro.Models
+{
+    public class UsuarioModel
+    {
+
+        public int IDUsuario { get; set; }
+        public string? Nombre { get; set; }
+        public int Edad { get; set; }
+        public string? Email { get; set; }
+
+    }
+}
---
+++ Registro/Registro.sql
@@ -0,0 +1,19 @@
+﻿create database REGISTRO_MVC
+USE REGISTRO_MVC
+
+CREATE TABLE USUARIOS(
+	IDUsuario int identity(1,1),
+	Nombre varchar(50),
+	Edad int,
+	Correo varchar(50)
+)
+
+select * from USUARIOS;
+
+create procedure sp_registrar
+@Nombre varchar(50),
+@Edad int,
+@Correo varchar(50)
+as begin 
+insert into Usuarios values(@Nombre, @Edad, @Correo)
+end
